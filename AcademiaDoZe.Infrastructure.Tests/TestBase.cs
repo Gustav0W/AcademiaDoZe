3420140c25b2ff3e3f77ae93719552cb9b1eedd7
GustavoW <guvieirawalter@gmail.com>
2025-09-15 02:02:45 -0300

    Adicionar arquivos de projeto.

---
+++ AcademiaDoZe.Infrastructure.Tests/TestBase.cs
@@ -0,0 +1,30 @@
+﻿using AcademiaDoZe.Infrastructure.Data;
+namespace AcademiaDoZe.Infrastructure.Tests
+{
+    public abstract class TestBase
+    {
+        protected string ConnectionString { get; private set; }
+        protected DatabaseType DatabaseType { get; private set; }
+        protected TestBase()
+        {
+            var config = CreateSqlServerConfig();
+            //var config = CreateMySqlConfig();
+            ConnectionString = config.ConnectionString;
+            DatabaseType = config.DatabaseType;
+        }
+        private (string ConnectionString, DatabaseType DatabaseType) CreateSqlServerConfig()
+        {
+            var connectionString = "Server=GUSTAVOWALTNOTE\\SQLEXPRESS;Database=AcademiaDoZe_TESTES;Trusted_Connection=True;TrustServerCertificate=True;";
+
+            return (connectionString, DatabaseType.SqlServer);
+
+        }
+        private (string ConnectionString, DatabaseType DatabaseType) CreateMySqlConfig()
+        {
+            var connectionString = "Server=localhost;Database=AcademiaDoZe-db;User Id=root;Password=senhaforte;";
+
+            return (connectionString, DatabaseType.MySql);
+
+        }
+    }
+}
