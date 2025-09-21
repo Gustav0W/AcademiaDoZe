f074c70d59a33587aaa23fac8d61ada67b5b7a14
GustavoW <guvieirawalter@gmail.com>
2025-09-21 17:03:45 -0300

    feat: Modificado App.Xaml para Aplicar os temas

--- AcademiaDoZe.Presentation.AppMaui/App.xaml.cs
+++ AcademiaDoZe.Presentation.AppMaui/App.xaml.cs
@@ -1,14 +1,33 @@
-﻿namespace AcademiaDoZe.Presentation.AppMaui;
+﻿using AcademiaDoZe.Presentation.AppMaui.Message;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace AcademiaDoZe.Presentation.AppMaui;
 
 public partial class App : Microsoft.Maui.Controls.Application
 {
     public App()
     {
         InitializeComponent();
+        AplicarTema();
+
+        WeakReferenceMessenger.Default.Register<TemaPreferencesUpdatedMessage>(this, (r, m) =>
+        {
+            AplicarTema();
+        });
     }
 
     protected override Window CreateWindow(IActivationState? activationState)
     {
         return new Window(new AppShell());
+    }
+
+    private void AplicarTema()
+    {
+        UserAppTheme = Preferences.Get("Tema", "system") switch
+        {
+            "light" => AppTheme.Light,
+            "dark" => AppTheme.Dark,
+            _ => AppTheme.Unspecified,
+        };
     }
 }
