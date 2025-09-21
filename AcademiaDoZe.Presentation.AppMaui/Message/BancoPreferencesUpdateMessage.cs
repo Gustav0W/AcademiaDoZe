65ff9528ad3d1145500010e3a0e240bf0ff6ab23
GustavoW <guvieirawalter@gmail.com>
2025-09-21 16:29:28 -0300

    feat: Implementa ConfigPage nas Views

---
+++ AcademiaDoZe.Presentation.AppMaui/Message/BancoPreferencesUpdateMessage.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace AcademiaDoZe.Presentation.AppMaui.Message;
+
+public sealed class BancoPreferencesUpdateMessage(string value) : ValueChangedMessage<string>(value)    
+{
+}
---
+++ AcademiaDoZe.Presentation.AppMaui/Views/ConfigPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AcademiaDoZe.Presentation.AppMaui.Message;
+using CommunityToolkit.Mvvm.Messaging;
+
+namespace AcademiaDoZe.Presentation.AppMaui.Views;
+
+public partial class ConfigPage : ContentPage
+{
+    public ConfigPage()
+    {
+        InitializeComponent();
+        CarregarTema();
+    }
+
+    private void CarregarTema()
+    {
+        TemaPicker.SelectedIndex = Preferences.Get("Tema", "system") switch { "light" => 0, "dark" => 1, _ => 2, };
+    }
+
+    private async void OnSalvarTemaClicked(object sender, EventArgs e)
+    {
+        string selectedTheme = TemaPicker.SelectedIndex switch { 0 => "light", 1 => "dark", _ => "system" };
+        Preferences.Set("Tema", selectedTheme);
+        
+        WeakReferenceMessenger.Default.Send(new TemaPreferencesUpdatedMessage("TemaAlterado"));
+        
+        await DisplayAlert("Sucesso", "Dados salvos com sucesso!", "OK");
+        
+        await Shell.Current.GoToAsync("//dashboard");
+    }
+    private async void OnCancelarClicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("//dashboard");
+    }
+    protected override void OnDisappearing()
+    {
+        base.OnDisappearing();
+        
+        WeakReferenceMessenger.Default.UnregisterAll(this);
+    }
+}
