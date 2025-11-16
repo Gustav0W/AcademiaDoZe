da9f46be464faa3dcf83a580f29958bea4e8fef3
GustavoW <guvieirawalter@gmail.com>
2025-11-16 14:18:41 -0300

    Adiciona InverseBoolConverter

---
+++ AcademiaDoZe.Presentation.AppMaui/Converters/InverseBoolConverter.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+
+namespace AcademiaDoZe.Presentation.AppMaui.Converters;
+
+public class InverseBoolConverter : IValueConverter
+{
+    public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return !(value as bool? ?? false);
+    }
+
+    public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        return !(value as bool? ?? false);
+    }
+}
